fb93ddbb55952167b65b6ddce2b27cfda796d1a4
ciddo <cid.dominique@gmail.com>
2026-03-17 19:37:00 -0400

    L: fixing my lack of commit check:

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -8,3 +8,5 @@ printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
 grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
+
+### Adding additional code to generate an extra commit followed by git push
